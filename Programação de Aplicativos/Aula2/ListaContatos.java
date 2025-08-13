3d82240560c5026f4ce242abbe5938e78a7968e2
Yagame <Gustavo_guaresi@estudante.sesisenai.org.br>
2025-08-13 18:43:19 -0300

    Update ListaContatos.java

--- Programação de Aplicativos/Aula2/ListaContatos.java
+++ Programação de Aplicativos/Aula2/ListaContatos.java
@@ -6,7 +6,7 @@ public class ListaContatos {
         ArrayList<String> lista = new ArrayList<>();
         Scanner entrada = new Scanner(System.in);
 
-        Integer opcao = null;
+        Integer opcao;
 
         do {
             System.out.println("\n|== MENU DE CONTATOS ==");
@@ -18,64 +18,70 @@ public class ListaContatos {
             System.out.print("Escolha uma opção: ");
 
             opcao = entrada.nextInt();
-            entrada.nextLine(); 
+            entrada.nextLine(); // limpa o buffer
             System.out.println("Você selecionou a opção: -->  " + opcao);
 
             switch (opcao) {
-            case 1:
-            if (lista.isEmpty()) {
-            System.out.println("\nNenhum contato cadastrado.");
-            } else {
-            System.out.println("\nLista de contatos:");
-            for (String contato : lista) {
-            System.out.println("| - " + contato + " | ");
-            }
-            }break;
 
-            case 2:
-            System.out.print("\nDigite o nome do contato: ");
-            String nome = entrada.nextLine();
-            lista.add(nome);
-            System.out.println("Contato cadastrado com sucesso!!");
-            break;
+                case 1:
+                    if (lista.isEmpty()) {
+                        System.out.println("\nNenhum contato cadastrado.");
+                    } else {
+                        System.out.println("\nLista de contatos:");
+                        for (String contato : lista) {
+                            System.out.println("| - " + contato + " | ");
+                        }
+                    }
+                    break;
 
-            case 3:
-            if (lista.isEmpty()) {
-            System.out.println("\nNenhum contato para alterar.");
-            break;
-            }
-            System.out.print("Digite o nome do contato que deseja alterar: ");
-            String antigoNome = entrada.nextLine();
-            if (lista.contains(antigoNome)) {
-            System.out.print("Digite o novo nome: ");
-            String novoNome = entrada.nextLine();
-            int index;
-            if(lista.indexOf(novoNome)){
-            System.out.println("Nome alterado com sucesso!");
-            }
-            System.out.println("Contato alterado com sucesso!");
-            } else {
-            System.out.println("Contato não encontrado.");
-            }break;
+                case 2:
+                    System.out.print("\nDigite o nome do contato: ");
+                    String nome = entrada.nextLine();
+                    lista.add(nome);
+                    System.out.println("Contato cadastrado com sucesso!!");
+                    break;
 
-            case 4:
-            if (lista.isEmpty()) {
-            System.out.println("\nNenhum contato para remover.");
-            break;
-            }
-            System.out.print("Digite o nome do contato que deseja remover: ");
-            String nomeRemover = entrada.nextLine();
-            if (lista.remove(nomeRemover)) {
-            System.out.println("Contato removido com sucesso!");
-            } else {
-            System.out.println("Contato não encontrado.");
-            }break;
-            
-            case 0:
-            System.out.println("Você saiu...");
-            break;
-            default:
-            System.out.println("Opção inválida. Tente novamente.");
+                case 3:
+                    if (lista.isEmpty()) {
+                        System.out.println("\nNenhum contato para alterar.");
+                        break;
+                    }
+                    System.out.print("Digite o nome do contato que deseja alterar: ");
+                    String antigoNome = entrada.nextLine();
+
+                    if (lista.contains(antigoNome)) {
+                        System.out.print("Digite o novo nome: ");
+                        String novoNome = entrada.nextLine();
+
+                        int index = lista.indexOf(antigoNome); // posição do contato antigo
+                        lista.set(index, novoNome); // altera na lista
+
+                        System.out.println("Contato alterado com sucesso!");
+                    } else {
+                        System.out.println("Contato não encontrado.");
+                    }
+                    break;
+
+                case 4:
+                    if (lista.isEmpty()) {
+                        System.out.println("\nNenhum contato para remover.");
+                        break;
+                    }
+                    System.out.print("Digite o nome do contato que deseja remover: ");
+                    String nomeRemover = entrada.nextLine();
+                    if (lista.remove(nomeRemover)) {
+                        System.out.println("Contato removido com sucesso!");
+                    } else {
+                        System.out.println("Contato não encontrado.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Você saiu...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
             }
 
         } while (!opcao.equals(0));
